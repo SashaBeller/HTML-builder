ff69f9d618d67c98527c028665090f44b46a9749
Sasha <polina.skripeleva@icloud.com>
2023-05-09 01:24:33 +0200

    fix: fix bug in task #6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -8,10 +8,8 @@ const styleCssPathNew = path.join(projectDir, 'style.css');
 const assetsDir = path.join(__dirname, 'assets');
 const assetsDirNewCopy = path.join(projectDir, 'assets');
 
-const templateHtml = path.join(__dirname, 'template.html');
-const articlesPath = path.join(__dirname, 'components', 'articles.html');
-const footerPath = path.join(__dirname, 'components', 'footer.html');
-const headerPath = path.join(__dirname, 'components', 'header.html');
+const templateHtmlPath = path.join(__dirname, 'template.html');
+const componentsDir = path.join(__dirname, 'components');
 
 function createFolderWithCss() {
   fs.mkdir(projectDir, {recursive: true}, (err) => {
@@ -53,31 +51,49 @@ async function copyDir(source, destination) {
         await fsPromises.copyFile(sourcePath, destPath);
       }
     }
-
   } catch (error) {
     console.error(error);
   }
 }
 
-Promise.all([
-  fsPromises.readFile(templateHtml, 'utf8'),
-  fsPromises.readFile(headerPath, 'utf8'),
-  fsPromises.readFile(articlesPath, 'utf8'),
-  fsPromises.readFile(footerPath, 'utf8'),
-])
-  .then(([html, header, articles, footer]) => {
-    const result = html
-      .replace('{{header}}', header)
-      .replace('{{articles}}', articles)
-      .replace('{{footer}}', footer);
-
-    const outputPath = path.join(projectDir, 'index.html');
-    return fsPromises.writeFile(outputPath, result, 'utf8');
-  })
-  .catch((err) => {
+async function readFiles(folderPath) {
+  try {
+    const htmlParts = [];
+    const dirents = await fsPromises.readdir(folderPath, {withFileTypes: true});
+    for (const dirent of dirents) {
+      if (dirent.isFile() && path.parse(dirent.name).ext === '.html') {
+        const direntPath = path.join(folderPath, dirent.name);
+        const fileName = path.parse(dirent.name).name;
+        const htmlPart = {
+          name: fileName,
+          path: direntPath
+        };
+        htmlParts.push(htmlPart);
+      }
+    } 
+    return htmlParts;
+  } catch (err) {
     console.error(err);
-  });
+  }
+}
 
+fsPromises.readFile(templateHtmlPath, 'utf8')
+  .then(templateHtml => {
+    return readFiles(componentsDir)
+      .then(components => {
+        const promises = components.map(comp => {
+          return fsPromises.readFile(comp.path, 'utf8')
+            .then(file => {
+              templateHtml = templateHtml.replace(`{{${comp.name}}}`, file);
+            });
+        });
+        return Promise.all(promises);
+      })
+      .then(() => {
+        const outputPath = path.join(projectDir, 'index.html');
+        return fsPromises.writeFile(outputPath, templateHtml, 'utf8');
+      });
+  });
 
 createFolderWithCss();
 copyDir(assetsDir, assetsDirNewCopy);
